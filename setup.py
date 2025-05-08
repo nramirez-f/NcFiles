ab99291149d67bda74fab6b1b908d91c95c9f6b9
Nramirez <nramirez@uma.es>
2025-05-08 17:54:43 +0200

    build(src): initial code

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ncfiles",
+    version="0.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "netCDF4",
+    ],
+    author="Nramirez",
+    description="Library to save simulations on a NetCDF",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/nramirez-f/NcViewer",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
